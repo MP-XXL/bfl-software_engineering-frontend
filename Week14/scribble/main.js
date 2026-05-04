31402a37bc00ed4ab4113c2746a7a7f7031eeb20
MP-XXL <royceapera@yahoo.com>
2026-05-04 12:55:05 -0500

    Adds Scrible font change

--- Week14/scribble/main.js
+++ Week14/scribble/main.js
@@ -6,3 +6,27 @@ liveText.addEventListener("input", function(e) {
         output.innerText = e.target.value
     })
 })
+
+
+function changeFont(noteId, font) {
+    let changeFontBtn = document.getElementById(noteId);
+    let oldAttr = changeFontBtn.getAttribute("class");
+    let fontName = oldAttr.search("font-f") 
+    if(fontName != -1) {
+        oldAttr = oldAttr.split(" ");
+        oldAttr.pop()
+        oldAttr.push(font)
+        let styling = String(oldAttr);
+        styling = styling.replaceAll(",", " ")
+        changeFontBtn.setAttribute("class", styling)
+    }else {
+        oldAttr = Array(oldAttr)
+        oldAttr.push(font)
+        let styling = String(oldAttr);
+        styling = styling.replaceAll(",", " ")
+        changeFontBtn.setAttribute("class", styling)
+        
+    }
+
+   
+}
