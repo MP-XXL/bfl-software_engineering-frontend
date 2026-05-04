1193fd5f40bd70cf950f2ca2cb8cfd5d34c38d94
MP-XXL <royceapera@yahoo.com>
2026-05-04 12:13:51 -0500

    Adds Scrible

--- Week14/scribble/main.js
+++ Week14/scribble/main.js
@@ -1,8 +1,8 @@
-let textOutput = document.querySelector(".live-output");
-let italicOutput = document.querySelector("#italic");
+let textOutput = document.querySelectorAll(".live-output");
 let liveText = document.querySelector("#live-input");
 
 liveText.addEventListener("input", function(e) {
-    textOutput.innerText = e.target.value
-    italicOutput.innerText = e.target.value
+    textOutput.forEach((output) => {
+        output.innerText = e.target.value
+    })
 })
